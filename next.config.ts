b4ea8c63465037f8d7fd007584e96c92538c2dac
ProvLeon <providence.leonard@gmail.com>
2026-08-23 15:53:08 +0000

    refactor: remove mobile application directory and associated documentation assets

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
-const isPages = process.env.BUILD_FOR_PAGES === "true";
-
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
   output: "export",
-  ...(isPages
-    ? {
-        basePath: "/downlink",
-        assetPrefix: "/downlink",
-      }
-    : {}),
 };
 
 export default nextConfig;
